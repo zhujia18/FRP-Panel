1ea6290150ab4032ebf657f71e7f1f9aed50b44e
Jasper <443493410@qq.com>
2017-07-01 01:34:25 +0800

    初始化

---
+++ FRPPanel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FRPPanel
+//
+//  Created by Jasper on 2017/7/1.
+//  Copyright © 2017年 Jasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FRPPanel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FRPPanel
+//
+//  Created by Jasper on 2017/7/1.
+//  Copyright © 2017年 Jasper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
