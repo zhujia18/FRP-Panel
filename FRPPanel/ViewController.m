45d4cbaaa244057068d7df90912896212b09ebc7
Jasper <443493410@qq.com>
2017-07-01 02:56:19 +0800

    上传跑通接口的验证性代码

--- FRPPanel/ViewController.m
+++ FRPPanel/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *urlString = @"http://xxxxxxxxx";
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+    NSString *authStr = [NSString stringWithFormat:@"xxx:xxxxx"];
+    NSData *base64Data = [authStr dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *base64Str = [base64Data base64EncodedStringWithOptions:0];
+    NSString *authorization = [NSString stringWithFormat:@"Basic %@",base64Str];
+    [manager.requestSerializer setValue:authorization forHTTPHeaderField:@"Authorization"];
+    
+    [manager GET:urlString parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSDictionary *dataDic = [NSJSONSerialization JSONObjectWithData:responseObject options:0 error:nil];
+        NSLog(@"data = %@",dataDic);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error = %@",error);
+    }];
+    
 }
 
 
